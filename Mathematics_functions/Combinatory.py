87d6903765907a52a9636758f672174c427399bb
Ivan Nieto <ivang9913@gmail.com>
2022-12-04 20:53:39 -0500

    python-projects: update description combinatory_mn

--- Mathematics_functions/Combinatory.py
+++ Mathematics_functions/Combinatory.py
@@ -1,17 +1,21 @@
 import numpy as np
 
+
 def mC1(m):
 	if not((type(m) is int)):
 		raise TypeError('m must be an integers')
 	else:
 		return [list(row) for row in np.identity(m, dtype = int)]
 
+# Return a list of lists where each one of these represents a way to choose n elements of m.
+# Each list has 0 or 1. 0 presents the element was not be choosen and 1 that it was. 
+# Raise a TypeError if the m or n not satisfy the conditions of mCn
 def combinatory_mn(m,n):
 	
 	if not((type(m) is int) and (type(n) is int)):
 		raise TypeError('m and n must be both integers')
 	elif m < 0:
-		raise Exception('It was expected m as natural number')
+		raise TypeError('It was expected m as natural number')
 	elif n > m:
 		combinations = []
 	elif m == n:
